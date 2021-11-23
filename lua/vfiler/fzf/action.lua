7be90c4f038b022e5ca3ab8ac5b0638a794d9c19
th.obaland@gmail.com <th.obaland@gmail.com>
2021-11-23 14:54:38 +0900

    Add script files.

---
+++ lua/vfiler/fzf/action.lua
@@ -0,0 +1,6 @@
+local M = {}
+
+function M.files(context, view)
+end
+
+return M
---
+++ lua/vfiler/fzf/config.lua
@@ -0,0 +1,12 @@
+local core = require 'vfiler/core'
+
+local M = {}
+
+M.configs = {
+}
+
+function M.setup(configs)
+  return core.table.merge(M.configs, configs)
+end
+
+return M
