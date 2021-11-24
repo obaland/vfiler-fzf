6ec257709ff4de46e9bb6b6b02d9e8e1b9325a7f
th.obaland@gmail.com <th.obaland@gmail.com>
2021-11-24 21:43:56 +0900

    Commit for verification.

--- lua/vfiler/fzf/action.lua
+++ lua/vfiler/fzf/action.lua
@@ -1,6 +1,16 @@
+local vim = require 'vfiler/vim'
+local config = require 'vfiler/fzf/config'
+
 local M = {}
 
 function M.files(context, view)
+  local item = view:get_current()
+  local dir = item.isdirectory and item.path or item.parent.path
+
+  local options = config.fzf_options()
+  options.dir = dir
+
+  vim.fn['vfiler_fzf#files'](options)
 end
 
 return M
--- lua/vfiler/fzf/config.lua
+++ lua/vfiler/fzf/config.lua
@@ -1,12 +1,38 @@
 local core = require 'vfiler/core'
+local vim = require 'vfiler/vim'
 
 local M = {}
 
 M.configs = {
+  options = {
+    '--layout=reverse',
+  },
+
+  layout = {
+    window = {
+      width = 0.9,
+      height = 0.6,
+    },
+  },
 }
 
 function M.setup(configs)
   return core.table.merge(M.configs, configs)
+end
+
+function M.fzf_options()
+  local options = vim.to_vimdict({})
+
+  if M.configs.layout then
+    for key, value in pairs(M.configs.layout) do
+      options[key] = type(value) == 'table' and vim.to_vimdict(value) or value
+    end
+  end
+
+  if M.configs.options then
+    options.options = table.concat(M.configs.options, ' ')
+  end
+  return options
 end
 
 return M
